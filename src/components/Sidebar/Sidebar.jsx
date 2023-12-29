f42bebc06c7009817aee5ae24bec922910f0d950
Dimitar Sabev <dimitarsabev00@gmail.com>
2023-12-29 22:38:58 +0200

    Fix Bug With Responsive In Sidebar Component

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -5,9 +5,8 @@ import { useNavigate } from "react-router";
 import { categories } from "../../utilities";
 import SidebarItem from "./SidebarItem";
 
-const Sidebar = ({mobileMenu, setMobileMenu}) => {
+const Sidebar = ({ mobileMenu, setMobileMenu }) => {
   const [selectedCategory, setSelectedCategory] = useState("New");
-
 
   const navigate = useNavigate();
 
@@ -25,9 +24,7 @@ const Sidebar = ({mobileMenu, setMobileMenu}) => {
   };
   return (
     <div
-      className={`md:block w-[240px] overflow-y-auto h-full py-4 bg-black absolute md:relative z-10 translate-x-[-240px] md:translate-x-0 transition-all ${
-        mobileMenu ? "translate-x-0" : ""
-      }`}
+      className={` w-[240px] overflow-y-auto h-full py-4 bg-black relative z-10`}
     >
       <div className="flex px-5 flex-col">
         {categories.map((item) => {
