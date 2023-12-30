07a9590c7626ad4f74d66078b0b5b3438fd614bd
Dimitar Sabev <dimitarsabev00@gmail.com>
2023-12-30 12:30:32 +0200

    Added fetchSelectedCategoryData Functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
 import "./App.css";
 import { BrowserRouter } from "react-router-dom";
 import { Routes } from "./components";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setGeneralFields, startLoading, stopLoading } from "./store";
+import { fetchDataFromApi } from "./utilities/api";
 
 const App = () => {
+  const { selectedCategory } = useSelector(({ generalSlice }) => generalSlice);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    fetchSelectedCategoryData(selectedCategory);
+  }, [selectedCategory]);
+
+  const fetchSelectedCategoryData = (query) => {
+    dispatch(startLoading());
+    fetchDataFromApi(`search/?q=${query}`).then(({ contents: results }) => {
+      dispatch(setGeneralFields({ categoryResult: results }));
+      dispatch(stopLoading());
+    });
+  };
   return (
     <BrowserRouter>
       <div className="flex flex-col h-full">
