f33d38329b506e98277ab3caa444d24b572fb81d
Dimitar Sabev <dimitarsabev00@gmail.com>
2024-01-21 19:52:18 +0200

    Use Another API_KEY

--- src/utilities/constants.ts
+++ src/utilities/constants.ts
@@ -1,2 +1,4 @@
 export const BASE_URL = "https://youtube.googleapis.com/youtube/v3";
-export const API_KEY = "AIzaSyBN3mGnxRhVpdGRVrZxNHzWCkbOGm90bqU";
+// my main account API_KEY
+// export const API_KEY = "AIzaSyBkg7FxJTizltPePzGR4Wib1sZgUftDwUc";
+export const API_KEY = "AIzaSyDSK1D2CnCSuIlz14g4jFqHS6KUGcSKuLo";
