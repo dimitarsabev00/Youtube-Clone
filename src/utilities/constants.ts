0a13aa971a0f2283d1ae9faee0a7e38facd347b1
Dimitar Sabev <dimitarsabev00@gmail.com>
2024-01-22 07:39:25 +0200

    Fix API_KEY In constants.ts In utilities Folder

--- src/utilities/constants.ts
+++ src/utilities/constants.ts
@@ -1,4 +1,3 @@
 export const BASE_URL = "https://youtube.googleapis.com/youtube/v3";
 // my main account API_KEY
-// export const API_KEY = "AIzaSyBkg7FxJTizltPePzGR4Wib1sZgUftDwUc";
-export const API_KEY = "AIzaSyDSK1D2CnCSuIlz14g4jFqHS6KUGcSKuLo";
+export const API_KEY = "AIzaSyBkg7FxJTizltPePzGR4Wib1sZgUftDwUc";
